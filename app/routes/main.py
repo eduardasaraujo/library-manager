cff68e4e50f2649d78207b26d0d55c534a923a58
eduardasaraujo1 <maria.00000856987@unicap.br>
2026-07-26 17:56:58 -0300

    feat: adicionando busca de livros por ISBN via API

--- app/routes/main.py
+++ app/routes/main.py
@@ -1,6 +1,8 @@
 from flask import Blueprint, render_template, request, redirect
 from app.models import Book
 from app import db
+from app.services.books_api import search_by_isbn
+from flask import jsonify
 
 main = Blueprint("main", __name__)
 
@@ -55,3 +57,13 @@ def edit_book(id):
         return redirect("/books")
 
     return render_template("books/edit_book.html", book=book)
+
+@main.route("/books/search/<isbn>")
+def search_book(isbn):
+
+    book = search_by_isbn(isbn)
+
+    if book is None:
+        return jsonify({"error": "Livro não encontrado"}), 404
+
+    return jsonify(book)
