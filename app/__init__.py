ff35579db8b94747013e8c359d507b4f6287562e
eduardasaraujo1 <maria.00000856987@unicap.br>
2026-07-27 09:39:45 -0300

    feat: implementando autenticação de usuários

--- app/__init__.py
+++ app/__init__.py
@@ -1,18 +1,34 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_bcrypt import Bcrypt
 
 db = SQLAlchemy()
+bcrypt = Bcrypt()
+
+login_manager = LoginManager()
+login_manager.login_view = "auth.login"
+
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object("config.Config")
 
     db.init_app(app)
+    bcrypt.init_app(app)
+    login_manager.init_app(app)
 
-    from app.models import User
+    from app.models import User, Book
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
 
     from app.routes.main import main
     app.register_blueprint(main)
+
+    from app.auth.routes import auth
+    app.register_blueprint(auth)
 
     with app.app_context():
         db.create_all()
---
+++ app/auth/routes.py
@@ -0,0 +1,65 @@
+from flask import Blueprint, render_template, request, redirect, url_for, flash
+from flask_login import login_user, logout_user, login_required
+from app import db, bcrypt
+from app.models import User
+
+auth = Blueprint("auth", __name__)
+
+@auth.route("/register", methods=["GET", "POST"])
+def register():
+
+    print("Método recebido:", request.method)
+
+    if request.method == "POST":
+        print("ENTROU NO POST")
+
+        password = bcrypt.generate_password_hash(
+            request.form["password"]
+        ).decode("utf-8")
+
+        user = User(
+            name=request.form["name"],
+            email=request.form["email"],
+            password=password
+        )
+
+        db.session.add(user)
+        db.session.commit()
+
+        flash("Usuário cadastrado com sucesso!", "success")
+
+        return redirect(url_for("auth.login"))
+
+    return render_template("auth/register.html")
+
+
+@auth.route("/login", methods=["GET", "POST"])
+def login():
+
+    if request.method == "POST":
+
+        user = User.query.filter_by(
+            email=request.form["email"]
+        ).first()
+
+        if user and bcrypt.check_password_hash(
+            user.password,
+            request.form["password"]
+        ):
+
+            login_user(user)
+
+            return redirect(url_for("main.books"))
+
+        flash("E-mail ou senha inválidos.", "danger")
+
+    return render_template("auth/login.html")
+
+
+@auth.route("/logout")
+@login_required
+def logout():
+
+    logout_user()
+
+    return redirect(url_for("auth.login"))
